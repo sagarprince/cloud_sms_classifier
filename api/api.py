ddbd55f5e0f104f79a8cb9a9afbc1dd868cadce5
sagarpansare2108 <136437479+sagarpansare2108@users.noreply.github.com>
2024-12-26 01:29:11 +0530

    commit changes.

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 # Load the model and vectorizer
 model = joblib.load('sms_classifier_model.pkl')
@@ -12,10 +13,24 @@ app = FastAPI()
 # Define the input data format
 class SMSInput(BaseModel):
     text: str
+    
+class SMSBatchInput(BaseModel):
+    messages: List[SMSInput]
 
 # Define the prediction endpoint
-@app.post("/predict")
+@app.post("/predict-message-type")
 def predict_sms(data: SMSInput):
     sms_vectorized = vectorizer.transform([data.text])
     prediction = model.predict(sms_vectorized)[0]
     return {"prediction": prediction}
+
+@app.post("/predict-messages-type")
+def predict_sms(data: SMSBatchInput):
+    sms_texts = [sms.text for sms in data.messages]
+    sms_vectorized = vectorizer.transform(sms_texts)
+    predictions = model.predict(sms_vectorized)
+    
+    result = [{"sms": sms_text, "type": pred} 
+              for sms_text, pred in zip(sms_texts, predictions)]
+    
+    return result
