db89d84e2dec68dc424fa60589d640266fa717a8
sagarpansare2108 <136437479+sagarpansare2108@users.noreply.github.com>
2024-12-26 16:25:24 +0530

    commit changes.

--- api/api.py
+++ api/api.py
@@ -2,10 +2,13 @@ import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import numpy as np
 
 # Load the model and vectorizer
 model = joblib.load('sms_classifier_model.pkl')
 vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+threshold = 0.9
 
 # Create the FastAPI app
 app = FastAPI()
@@ -19,18 +22,41 @@ class SMSBatchInput(BaseModel):
 
 # Define the prediction endpoint
 @app.post("/predict-message-type")
-def predict_sms(data: SMSInput):
+def predict_message_type(data: SMSInput):
     sms_vectorized = vectorizer.transform([data.text])
-    prediction = model.predict(sms_vectorized)[0]
-    return {"prediction": prediction}
+    # Predict Probabilities
+    probabilities = model.predict_proba(sms_vectorized)
+    max_prob = np.max(probabilities)
+    classified_type = model.predict(sms_vectorized)[0]
+    type = 'unknown'
+    if max_prob >= threshold:
+        type = classified_type
+    return {"type": type}
 
 @app.post("/predict-messages-type")
-def predict_sms(data: SMSBatchInput):
+def predict_messages_type(data: SMSBatchInput):
     sms_texts = [sms.text for sms in data.messages]
     sms_vectorized = vectorizer.transform(sms_texts)
     predictions = model.predict(sms_vectorized)
+    probabilities = model.predict_proba(sms_vectorized)
+    # result = [{"sms": sms_text, "type": type} 
+    #           for sms_text, pred in zip(sms_texts, predictions)]
     
-    result = [{"sms": sms_text, "type": pred} 
-              for sms_text, pred in zip(sms_texts, predictions)]
+    # Map predictions to types
+    result = []
+    for sms_text, pred, prob in zip(sms_texts, predictions, probabilities):
+        max_prob = np.max(prob)  # Get the maximum probability
+        type = "unknown"
+        
+        # Check the threshold
+        if max_prob >= threshold:
+            type =pred
+        
+        # Append the result
+        result.append({
+            "text": sms_text,
+            "type": type,
+            "probability": max_prob
+        })
     
     return result
